3af1c404ecb1afc28e4b7f8763dcdda6514498d2
YednapNibas <pandeyfcb7849@gmail.com>
2019-06-07 12:40:53 +0545

    product-data initialized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductDataComponent } from './product-data/product-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductDataComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/product-data/product-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-data',
+  templateUrl: './product-data.component.html',
+  styleUrls: ['./product-data.component.css']
+})
+export class ProductDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
