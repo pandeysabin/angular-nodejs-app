09efd7e24cf404223ff035b7422ee743255ea237
YednapNibas <pandeyfcb7849@gmail.com>
2019-07-18 11:37:56 +0545

    application upgraded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductDataComponent } from './product-data/product-data.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'list', component: ProductDataComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
